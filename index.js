02f403111e7f88c8606a686c3fcb10fb2831158b
Sheilah <sheilahndiema@gmail.com>
2022-08-18 19:17:53 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    const hq = 42;
+    return Math.abs(hq - pickup)
+}
+function distanceFromHqInFeet(pickup) {
+    distanceFromHqInBlocks(pickup);
+    let distanceInFeet = distanceFromHqInBlocks(pickup) * 264
+    return distanceInFeet;
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination > start) {
+
+        return (destination - start) * 264;
+    }
+    else { return (start - destination) * 264 }
+}
+
+//FARE PRICE
+
+function calculatesFarePrice(start, destination) {
+    let distPrice = (parseInt((distanceTravelledInFeet(start, destination)), 10))
+
+    if (distPrice <= 400) {
+        return 0;
+    } else if (distPrice <= 2000) {
+        return ((distPrice - 400) * 0.02)
+    } else if (distPrice <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
